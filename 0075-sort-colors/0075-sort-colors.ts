fb18aaf056e9e9d163bf542d09c660db59635f27
hasferrr <hasbi.f.hamd@gmail.com>
2024-05-20 13:19:16 +0700

    Time: 50 ms (91.13%), Space: 51.8 MB (14.44%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.ts
+++ 0075-sort-colors/0075-sort-colors.ts
@@ -1,22 +1,46 @@
 function sortColors(nums: number[]): void {
-  // Bucket sort
-  const map = [0, 0, 0]
-
-  let i = 0
-  while (i < nums.length) {
-    map[nums[i]] = map[nums[i]] + 1
-    i++
+  // partitioning (quicksort)
+  const swap = (i1: number, i2: number): void => {
+    [nums[i1], nums[i2]] = [nums[i2], nums[i1]]
   }
 
-  i = 0
-  let indexNums = 0
-  while (i < map.length) {
-    let j = 0
-    while (j < map[i]) {
-      nums[indexNums] = i
-      indexNums++
-      j++
+  const pivot = 1
+  let i = 0
+  let left = 0
+  let right = nums.length - 1
+  while (i <= right) {
+    if (nums[i] < pivot) {
+      swap(left, i)
+      left++
+      i++
+    } else if (nums[i] > pivot) {
+      swap(right, i)
+      right--
+    } else {
+      i++
     }
-    i++
   }
 }
+
+// function sortColors(nums: number[]): void {
+//   // Bucket sort
+//   const map = [0, 0, 0]
+
+//   let i = 0
+//   while (i < nums.length) {
+//     map[nums[i]] = map[nums[i]] + 1
+//     i++
+//   }
+
+//   i = 0
+//   let indexNums = 0
+//   while (i < map.length) {
+//     let j = 0
+//     while (j < map[i]) {
+//       nums[indexNums] = i
+//       indexNums++
+//       j++
+//     }
+//     i++
+//   }
+// }
