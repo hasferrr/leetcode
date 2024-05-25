a6e0cedb0ba6ddf238084100adeb6e061a77d6f9
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-05-25 11:51:46 +0700

    Time: 89 ms (22.61%), Space: 56.6 MB (51.16%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrder(root: TreeNode | null): number[][] {
+  if (!root) return []
+  const ans: number[][] = []
+  const queue: TreeNode[] = [root]
+  while (queue.length) {
+    const level: number[] = []
+    const qLen = queue.length
+    for (let i = 0; i < qLen; i++) {
+      const node = queue.shift()
+      level.push(node.val)
+      if (node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right)
+    }
+    ans.push(level)
+  }
+  return ans
+}
