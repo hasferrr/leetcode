ae801a84d213aaf32ee781ff9e19203f73b3420f
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-06-01 19:41:48 +0700

    Time: 172 ms (89.06%), Space: 62.8 MB (92.71%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.ts
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.ts
@@ -1,17 +1,62 @@
-// import { PriorityQueue } from '@datastructures-js/priority-queue'
+function kClosest(points: number[][], k: number): number[][] { 
+  const toOrigin = (p: number[]) => p[0] ** 2 + p[1] ** 2
 
-function kClosest(points: number[][], k: number): number[][] {
-  type Point = [number, number]
-  const toOrigin = (p: Point) => p[0]**2 + p[1]**2
-  const pointsMinHeap = new PriorityQueue({
-    compare: (p1: Point, newP: Point) => toOrigin(newP) < toOrigin(p1) ? 1 : -1
-  })
-  for (const p of points) {
-    pointsMinHeap.enqueue(p)
+  const swap = (i1: number, i2: number) => {
+    [points[i1], points[i2]] = [points[i2], points[i1]]
   }
-  const result: Point[] = []
-  for (; k > 0; k--) {
-    result.push(pointsMinHeap.dequeue())
+
+  const shiftUp = (i: number) => {
+    if (i === 0) return
+    
+    const parentIndex = Math.floor((i - 1) / 2)
+    if (toOrigin(points[i]) < toOrigin(points[parentIndex])) {
+      swap(i, parentIndex)
+      return shiftUp(parentIndex)
+    }
   }
-  return result
+
+  const shiftDown = (i: number) => {
+    const childL = 2 * i + 1
+    const childR = 2 * i + 2
+
+    if (childL >= points.length && childR >= points.length) {
+      return
+    }
+
+    let childIndex = childL
+
+    if (childR < points.length && toOrigin(points[childL]) > toOrigin(points[childR])) {
+      childIndex =  childR
+    }
+
+    if (toOrigin(points[i]) > toOrigin(points[childIndex])) {
+      swap(i, childIndex)
+      return shiftDown(childIndex)
+    }
+  }
+
+  const heapifyByDistanceToOrigin = () => {
+    for (let i = Math.floor((points.length - 1) / 2); i > -1; i--) {
+      shiftDown(i)
+    }
+  }
+
+  const pop = (): number[] => {
+    const [x, y] = points[0]
+    swap(0, points.length - 1)
+    points.pop()
+    shiftDown(0)
+    return [x, y]
+  }
+
+  const main = () => {
+    heapifyByDistanceToOrigin()
+    const result: number[][] = []
+    for (; k > 0; k--) {
+      result.push(pop())
+    }
+    return result
+  }
+
+  return main()
 }
