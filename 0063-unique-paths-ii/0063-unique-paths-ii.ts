e590e6754d42989135e0eb9db51038d0abe32fb3
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-06-09 16:07:08 +0700

    Time: 49 ms (96.50%), Space: 51.6 MB (74.00%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.ts
+++ 0063-unique-paths-ii/0063-unique-paths-ii.ts
@@ -1,42 +1,73 @@
-// Tabulation (Bottom-up)
-// Time: O(row + col)
-// Space: O(1)
+// Tabulation (Bottom-up) v2
+// Time: O(m + n)
+// Space: O(n)
 function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
   const ROWS = obstacleGrid.length
   const COLS = obstacleGrid[0].length
+
   if (obstacleGrid[0][0] === 1) return 0
   if (obstacleGrid[ROWS - 1][COLS - 1] === 1) return 0
 
-  let prevRow = Array(COLS).fill(0)
-  prevRow[COLS - 1] = 1
+  const dp = Array(COLS).fill(0)
+  dp[COLS - 1] = 1
 
   for (let i = ROWS - 1; i >= 0; i--) {
-    const currRow = Array(COLS)
-
     if (obstacleGrid[i][COLS - 1] === 1) {
-      currRow[COLS - 1] = 0
-    } else {
-      currRow[COLS - 1] = prevRow[COLS - 1]
+      dp[COLS - 1] = 0
     }
 
     for (let j = COLS - 2; j >= 0; j--) {
       if (obstacleGrid[i][j] === 1) {
-        currRow[j] = 0
+        dp[j] = 0
       } else {
-        currRow[j] = prevRow[j] + currRow[j + 1]
+        dp[j] += dp[j + 1]
       }
     }
-
-    prevRow = currRow
   }
 
-  return prevRow[0]
+  return dp[0]
 }
 
 
+// // Tabulation (Bottom-up) v1
+// // Time: O(m + n)
+// // Space: O(2 * n)
+// function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
+//   const ROWS = obstacleGrid.length
+//   const COLS = obstacleGrid[0].length
+//   if (obstacleGrid[0][0] === 1) return 0
+//   if (obstacleGrid[ROWS - 1][COLS - 1] === 1) return 0
+
+//   let prevRow = Array(COLS).fill(0)
+//   prevRow[COLS - 1] = 1
+
+//   for (let i = ROWS - 1; i >= 0; i--) {
+//     const currRow = Array(COLS)
+
+//     if (obstacleGrid[i][COLS - 1] === 1) {
+//       currRow[COLS - 1] = 0
+//     } else {
+//       currRow[COLS - 1] = prevRow[COLS - 1]
+//     }
+
+//     for (let j = COLS - 2; j >= 0; j--) {
+//       if (obstacleGrid[i][j] === 1) {
+//         currRow[j] = 0
+//       } else {
+//         currRow[j] = prevRow[j] + currRow[j + 1]
+//       }
+//     }
+
+//     prevRow = currRow
+//   }
+
+//   return prevRow[0]
+// }
+
+
 // // Memoization
-// // Time: O(row + col)
-// // Space: O(row + col)
+// // Time: O(m + n)
+// // Space: O(m + n)
 // function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
 //   const ROWS = obstacleGrid.length
 //   const COLS = obstacleGrid[0].length
@@ -66,8 +97,8 @@ function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
 
 
 // // Brute force
-// // Time: O(2^(row + col))
-// // Space: O(row + col)
+// // Time: O(2^(m + n))
+// // Space: O(m + n)
 // function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
 //   const ROWS = obstacleGrid.length
 //   const COLS = obstacleGrid[0].length
