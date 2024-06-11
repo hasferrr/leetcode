7d29cd19b28b038a9c3fb88c56ae4ae1428cd665
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-06-11 09:44:18 +0700

    Time: 56 ms (85.38%), Space: 51.1 MB (97.18%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.ts
+++ 0191-number-of-1-bits/0191-number-of-1-bits.ts
@@ -1,8 +1,18 @@
 function hammingWeight(n: number): number {
   let count = 0
   while (n > 0) {
-    count += n & 1
-    n = n >> 1
+    // take the right most ones
+    n = n & (n - 1)
+    count++
   }
   return count
 }
+
+// function hammingWeight(n: number): number {
+//   let count = 0
+//   while (n > 0) {
+//     count += n & 1
+//     n = n >> 1
+//   }
+//   return count
+// }
