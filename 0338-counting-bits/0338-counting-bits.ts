d0e56179d8bc262367f5a69788b327f0c0449215
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-06-11 10:16:22 +0700

    Time: 71 ms (81.82%), Space: 56.7 MB (79.55%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.ts
+++ 0338-counting-bits/0338-counting-bits.ts
@@ -1,15 +1,34 @@
+// O(n)
 function countBits(n: number): number[] {
-  const result = []
-  let m = 0
-  while (m <= n) {
-    let ones = 0
-    let k = m
-    while (k !== 0) {
-      ones += k & 1
-      k = k >> 1
+  const dp = Array(n + 1).fill(0)
+  
+  let nextBit = 1
+  while (true) {
+    const doubleNextBit = nextBit * 2
+    for (let i = nextBit; i < doubleNextBit; i++) {
+      if (i >= n + 1) {
+        return dp
+      }
+      dp[i] = dp[i - nextBit] + 1
     }
-    result.push(ones)
-    m++
+    nextBit = doubleNextBit
   }
-  return result
 }
+
+
+// O(n log n)
+// function countBits(n: number): number[] {
+//   const result = []
+//   let m = 0
+//   while (m <= n) {
+//     let ones = 0
+//     let k = m
+//     while (k !== 0) {
+//       ones += k & 1
+//       k = k >> 1
+//     }
+//     result.push(ones)
+//     m++
+//   }
+//   return result
+// }
