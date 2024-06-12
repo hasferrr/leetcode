45cf1cf991fb65f6419421a7df500bf74a3e5576
hasferrr <hasferrr@proton.me>
2024-06-12 22:30:18 +0700

    Time: 68 ms (57.01%), Space: 55.1 MB (33.51%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,14 +1,18 @@
 function isPalindrome(s: string): boolean {
+  const isLowerCase = (c) => c.charCodeAt() >= 97 && c.charCodeAt() <= 122
+  const isNumeric = (c) => c.charCodeAt() >= 48 && c.charCodeAt() <= 57
+
   let l = 0
   let r = s.length - 1
+
   while (l < r) {
     const str1 = s[l].toLowerCase()
     const str2 = s[r].toLowerCase()
-    if (isNaN(parseInt(str1)) && str1 === s[l].toUpperCase()) {
+    if (!isLowerCase(str1) && !isNumeric(str1)) {
       l++
       continue
     }
-    if (isNaN(parseInt(str2)) && str2 === s[r].toUpperCase()) {
+    if (!isLowerCase(str2) && !isNumeric(str2)) {
       r--
       continue
     }
