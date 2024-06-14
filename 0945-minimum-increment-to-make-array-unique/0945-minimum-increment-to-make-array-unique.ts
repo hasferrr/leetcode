34f20b57f8ef8367291fe68e15d926f396567767
hasferrr <hasferrr@proton.me>
2024-06-14 21:41:20 +0700

    Time: 207 ms (100.00%), Space: 61.7 MB (100.00%) - LeetHub

---
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.ts
@@ -0,0 +1,13 @@
+function minIncrementForUnique(nums: number[]): number {
+  nums.sort((a, b) => a - b)
+  let count = 0
+  for (let i = 1; i < nums.length; i++) {
+    let prev = nums[i - 1]
+    let num = nums[i]
+    if (num <= prev) {
+      nums[i] = prev + 1
+      count += nums[i] - num
+    }
+  }
+  return count
+}
