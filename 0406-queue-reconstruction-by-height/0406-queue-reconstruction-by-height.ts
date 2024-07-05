6a983bcd28aa274794b5567403dbeb86dba85828
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-07-05 13:52:32 +0700

    Time: 116 ms (12.90%), Space: 59.5 MB (9.68%) - LeetHub

--- 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.ts
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.ts
@@ -1,57 +1,129 @@
-class Node {
-  val: number[]
-  next: Node | null
-  constructor(val: number[], next: undefined | Node) {
-    this.val = val
-    this.next = next ?? null
-  }
-}
+// Segment Tree - O(n log n)
 
-class LinkedList {
-  head: Node | null
-  constructor() {
-    this.head = null
+class SegmentTree {
+  left: SegmentTree | null
+  right: SegmentTree | null
+  start: number
+  end: number
+  count: number
+  constructor(start: number, end: number) {
+    this.build(start, end)
   }
-  insert(index: number, val: number[]) {
-    let prev = null
-    let curr = this.head
-    let i = 0
-    while (i < index) {
-      prev = curr
-      curr = curr.next
-      i++
+
+  private build(start: number, end: number) {
+    this.left = null
+    this.right = null
+    this.start = start
+    this.end = end
+
+    if (start === end) {
+      this.count = 1
+      return
     }
-    if (!prev) {
-      this.head = new Node(val, curr)
+
+    const mid = Math.floor((start + end) / 2)
+    this.left = new SegmentTree(start, mid)
+    this.right = new SegmentTree(mid + 1, end)
+    this.count = this.left.count + this.right.count
+  }
+
+  queryAndUpdate(k: number) {
+    if (this.start === this.end) {
+      this.count--
+      return this.start
+    }
+
+    this.count--
+    if (this.left.count > k) {
+      return this.left.queryAndUpdate(k)
     } else {
-      prev.next = new Node(val, curr)
+      return this.right.queryAndUpdate(k - this.left.count)
     }
-  }
-  toArray() {
-    const res = []
-    let curr = this.head
-    while (curr) {
-      res.push(curr.val)
-      curr = curr.next
-    }
-    return res
   }
 }
 
 function reconstructQueue(people: number[][]): number[][] {
-  // Sort people
-  // people unsorted    = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
-  // people sorted      = [[7,0],[7,1],[6,1],[5,0],[5,2],[4,4]]
+  // O(n log n)
   people.sort((a, b) => a[0] !== b[0]
-    ? b[0] - a[0]
-    : a[1] - b[1])
+    ? a[0] - b[0]
+    : b[1] - a[1])
+  console.log(people)
 
-  // For every person, insert it to their k index
-  // after insertion    = [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
-  const root = new LinkedList()
+  const result = Array(people.length)
+
+  // O(n)
+  const tree = new SegmentTree(0, people.length - 1)
+
+  // O(n log n)
   for (const p of people) {
-    root.insert(p[1], p)
+    const [h, k] = p
+    const index = tree.queryAndUpdate(k)
+    result[index] = p
   }
   
-  return root.toArray()
+  return result
 }
+
+
+// Linked List - O(n^2)
+
+// class Node {
+//   val: number[]
+//   next: Node | null
+//   constructor(val: number[], next: undefined | Node) {
+//     this.val = val
+//     this.next = next ?? null
+//   }
+// }
+
+// class LinkedList {
+//   head: Node | null
+//   constructor() {
+//     this.head = null
+//   }
+//   insert(index: number, val: number[]) {
+//     let prev = null
+//     let curr = this.head
+//     let i = 0
+//     while (i < index) {
+//       prev = curr
+//       curr = curr.next
+//       i++
+//     }
+//     if (!prev) {
+//       this.head = new Node(val, curr)
+//     } else {
+//       prev.next = new Node(val, curr)
+//     }
+//   }
+//   toArray() {
+//     const res = []
+//     let curr = this.head
+//     while (curr) {
+//       res.push(curr.val)
+//       curr = curr.next
+//     }
+//     return res
+//   }
+// }
+
+// function reconstructQueue(people: number[][]): number[][] {
+//   // Sort people
+//   // people unsorted    = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+//   // people sorted      = [[7,0],[7,1],[6,1],[5,0],[5,2],[4,4]]
+//   // O(n log n)
+//   people.sort((a, b) => a[0] !== b[0]
+//     ? b[0] - a[0]
+//     : a[1] - b[1])
+
+//   // For every person, insert it to their k index
+//   // after insertion    = [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
+//   // O(n^2)
+//   const root = new LinkedList()
+//   for (const p of people) {
+//     root.insert(p[1], p)
+//   }
+  
+//   // O(n)
+//   return root.toArray()
+// }
