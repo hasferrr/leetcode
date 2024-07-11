9b49aaa4492255dda72559042349513b631ad856
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-07-11 15:50:24 +0700

    Time: 63 ms (94.53%), Space: 53.6 MB (91.80%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.ts
+++ 0090-subsets-ii/0090-subsets-ii.ts
@@ -1,21 +1,19 @@
 function subsetsWithDup(nums: number[]): number[][] {
+  nums.sort((a, b) => a - b)
   const result: number[][] = []
-  const dfs = (i: number, subset: number[], skip: Set<number>) => {
+  const dfs = (i: number, subset: number[]) => {
     if (i === nums.length) {
       result.push([...subset])
       return
     }
-    if (skip.has(nums[i])) {
-      dfs(i + 1, subset, skip)
-      return
-    }
     subset.push(nums[i])
-    dfs(i + 1, subset, skip)
-    const numSkip = subset.pop()
-    skip.add(numSkip)
-    dfs(i + 1, subset, skip)
-    skip.delete(numSkip)
+    dfs(i + 1, subset)
+    subset.pop()
+    while (i + 1 < nums.length && nums[i] === nums[i + 1]) {
+      i++
+    }
+    dfs(i + 1, subset)
   }
-  dfs(0, [], new Set())
+  dfs(0, [])
   return result
 }
