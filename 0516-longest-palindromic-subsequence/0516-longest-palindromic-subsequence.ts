b24337e38e1483c5f448eb923b5a2ee1c60b0c42
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-08-12 14:33:37 +0700

    Time: 172 ms (78.57%), Space: 76 MB (58.57%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.ts
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.ts
@@ -1,30 +1,18 @@
 function longestPalindromeSubseq(s: string): number {
-  let longest = 1
   const memo = Array.from({ length: s.length }, () => Array(s.length))
   const subseq = (l: number, r: number): number => {
-    if (l < 0 || r === s.length) {
-      return 0
-    }
-    if (memo[l][r] !== undefined) {
-      return memo[l][r]
-    }
+    if (l > r) return 0
+    if (l === r) return 1
+    if (memo[l][r] !== undefined) return memo[l][r]
     if (s[l] === s[r]) {
-      memo[l][r] = (l === r ? 1 : 2) + subseq(l - 1, r + 1)
+      memo[l][r] = 2 + subseq(l + 1, r - 1)
       return memo[l][r]
     }
     memo[l][r] = Math.max(
-      subseq(l - 1, r),
-      subseq(l, r + 1),
+      subseq(l + 1, r),
+      subseq(l, r - 1),
     )
     return memo[l][r]
   }
-  for (let i = 0; i < s.length; i++) {
-    longest = Math.max(
-      longest,
-      subseq(i, i),
-      subseq(i - 1, i),
-      subseq(i, i + 1),
-    )
-  }
-  return longest
+  return subseq(0, s.length - 1)
 }
