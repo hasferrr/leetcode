0ba67320d84f8cb6b403d990d0a81134250026b7
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-09-03 09:52:40 +0700

    Time: 54 ms (90.59%), Space: 53.1 MB (18.83%) - LeetHub

--- 0139-word-break/0139-word-break.ts
+++ 0139-word-break/0139-word-break.ts
@@ -1,65 +1,46 @@
-class TrieNode {
-  isWord: boolean
-  children: Map<string, TrieNode>
-  constructor() {
-    this.isWord = false
-    this.children = new Map()
-  }
-}
-
-class Trie {
-  root: TrieNode
-  constructor() {
-    this.root = new TrieNode()
-  }
-
-  add(word: string): void {
-    let curr = this.root
-    for (const c of word) {
-      if (!curr.children.has(c)) {
-        curr.children.set(c, new TrieNode())
-      }
-      curr = curr.children.get(c)
-    }
-    curr.isWord = true
-  }
-
-  suffixInTrie(word: string, index: number): number[] {
-    let endIndexList: number[] = []
-    let curr = this.root
-
-    for (let i = index; i < word.length; i++) {
-      const c = word[i]
-      if (!curr.children.has(c)) {
-        return endIndexList
-      }
-      curr = curr.children.get(c)
-      if (curr.isWord) {
-        endIndexList.push(i + 1)
-      }
-    }
-
-    return endIndexList
-  }
-}
-
 function wordBreak(s: string, wordDict: string[]): boolean {
-  const trie = new Trie()
-  for (const word of wordDict) {
-    trie.add(word)
+  const dp: boolean[] = Array(s.length + 1)
+  dp[dp.length - 1] = true
+  for (let i = dp.length - 2; i >= 0; i--) {
+    for (const word of wordDict) {
+      let j = i
+      let k = 0
+      while (j < s.length && k < word.length) {
+        if (s[j] !== word[k]) break
+        j++
+        k++
+      }
+      if (k === word.length && dp[j]) {
+        dp[i] = true
+        break
+      }
+    }
+    dp[i] = dp[i] === true
   }
+  return dp[0]
+}
+
+
+// Memoization
+function wordBreak_memo(s: string, wordDict: string[]): boolean {
   const memo: boolean[] = Array(s.length)
-  const helper = (index: number): boolean => {
+  const helper = (index: number) => {
     if (index === s.length) {
       return true
     }
     if (memo[index] !== undefined) {
       return memo[index]
     }
-    const nextIndexList = trie.suffixInTrie(s, index)
-    for (const next of nextIndexList) {
-      if (helper(next)) {
-        memo[next] = true
+    for (const word of wordDict) {
+      let j = index
+      let k = 0
+      while (j < s.length && k < word.length) {
+        if (s[j] !== word[k]) break
+        j++
+        k++
+      }
+      if (k === word.length && helper(j)) {
+        memo[j] = true
         return true
       }
     }
@@ -68,22 +49,3 @@ function wordBreak(s: string, wordDict: string[]): boolean {
   }
   return helper(0)
 }
-
-
-// wordDict -> DS Trie
-//      012345
-// s = "applepenapple"
-//      ^
-//   wordDict =["apple", "pen"]
-// TRIE => { a: apple, p -> e -> t }
-// e -> b  
-
-
-// s = "apple  penapple"
-// wordDict = ["apple", "pen"]
-//
-// "appla"
-//  01234
-// wordDict = ["apple", "pen"]
-// i=5
-// endIndex=4
